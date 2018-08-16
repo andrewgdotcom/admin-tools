81897bc9251b68a6701f67d93d6d6b88b6be1a1c
Andrew Gallagher <andrew.gallagher@siren.io>
2018-08-16 12:02:53 +0100

    Extended refactor for namespace safety, use of STDERR

--- parse-opt.sh
+++ parse-opt.sh
@@ -1,8 +1,11 @@
-#####################################################################
+################################################################################
 # Extended getopt handler based on https://stackoverflow.com/a/29754866/1485960
 # This file has no magic number, and is not executable.
 # THIS IS INTENTIONAL as it should never be executed, only sourced.
-#####################################################################
+#
+# NB this script uses the __PO__ prefix on internal functions and variables
+# to avoid naming clash. DO NOT USE __PO__<*> anywhere in the calling script.
+################################################################################
 
 # PO_SHORT_MAP and PO_LONG_MAP must be declared in the calling script, e.g.:
 #
@@ -36,107 +39,128 @@
 
 ! getopt --test > /dev/null
 if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
-    echo "Enhanced getopt not found!"
+    echo "Enhanced getopt not found!" >&2
     exit 1
 fi
 
-__PO__set_var_with_default() {
-    local __PO__var_with_default="$1"
-    local __PO__value="$2"
+__PO__set_var() {
+    local var_with_default="$1"
+    local value="$2"
     # split on `=` into variable and default value.
-    # NB we do it the hard way to ensure __PO__default="" if no default given.
-    __PO__variable="${__PO__var_with_default%=*}"
-    __PO__default="${__PO__var_with_default#${__PO__variable}}"
-    __PO__default="${__PO__default#=}"
+    # NB we do it the hard way to ensure default="" if no default given.
+    local variable="${var_with_default%=*}"
+    local default="${var_with_default#${variable}}"
+    default="${default#=}"
     # if we have been passed a value, set the variable to it,
     # otherwise to the default (if that was provided)
     # NB the embedded quotes prevent eval from word-splitting the values
-    if [[ $__PO__value ]]; then
-        eval $__PO__variable='"$__PO__value"'
-    elif [[ $__PO__default ]]; then
-        eval $__PO__variable='"$__PO__default"'
+    if [[ $value ]]; then
+        echo $variable="'$value';"
+    elif [[ $default ]]; then
+        echo $variable="'$default';"
     fi
 }
 
-for __PO__key in "${!PO_SHORT_MAP[@]}"; do
-    if [[ "${PO_SHORT_MAP[$__PO__key]%=*}" != "${PO_SHORT_MAP[$__PO__key]}" && \
-        "${__PO__key%::}" == "${__PO__key}" ]]; then
-        # sanity failure!
-        echo "PANIC: non-optional key '$__PO__key' must not have a default value"
-        exit 4
-    fi
-done
-
-declare -A __PO__LONG_INVERSES
-for __PO__key in "${!PO_LONG_MAP[@]}"; do
-    if [[ "${PO_LONG_MAP[$__PO__key]%=*}" != "${PO_LONG_MAP[$__PO__key]}" && \
-        "${__PO__key%::}" == "${__PO__key}" ]]; then
-        # sanity failure!
-        echo "PANIC: non-optional key '$__PO__key' must not have a default value"
-        exit 4
-    fi
-    if [[ "$__PO__key" == "${__PO__key%:}" ]]; then
-        # key takes no value, therefore we can support no-<key>
-        __PO__LONG_INVERSES["no-$__PO__key"]="${PO_LONG_MAP[$__PO__key]}"
-    fi
-done
-
-# concatenate option hash keys and invoke enhanced getopt on ARGV
-! __PO__PARSED=$(getopt \
-    -o $(IFS="";echo "${!PO_SHORT_MAP[*]}") \
-    -l $(IFS=,;echo "${!PO_LONG_MAP[*]}","${!__PO__LONG_INVERSES[*]}") \
-    --name "$0" -- "$@")
-if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
-    exit 2
-fi
-
-# `set` reloads ARGV from __PO_PARSED; `--` forbids set from consuming options
-# `eval` parses the embedded single quotes in enhanced-getopt's output
-eval set -- "$__PO__PARSED"
-
-# we have now reconstituted ARGV in canonical form, so we can consume in order
-while true; do
-    if [[ "$1" == "--" ]]; then
-        # stop processing options
-        shift
-        break
-    fi
-    for __PO__key in "${!PO_SHORT_MAP[@]}"; do
-        # strip trailing colon(s)
-        __PO__opt="${__PO__key%%:*}"
-        __PO__variable="${PO_SHORT_MAP[$__PO__key]}"
-        if [[ "$1" == "-$__PO__opt" ]]; then
-            if [[ "$__PO__opt" == "$__PO__key" ]]; then
-                eval ${__PO__variable}="true"
-                shift
-                continue 2
-            else
-                __PO__set_var_with_default "${__PO__variable}" "$2"
-                shift 2
-                continue 2
-            fi
+__PO__canonicalize_argv() {
+    local key
+    for key in "${!PO_SHORT_MAP[@]}"; do
+        if [[ "${PO_SHORT_MAP[$key]%=*}" != "${PO_SHORT_MAP[$key]}" && \
+            "${key%::}" == "${key}" ]]; then
+            # sanity failure!
+            echo "PANIC: non-optional key '$key' must not have a default value" >&2
+            exit 4
         fi
     done
-    for __PO__key in "${!PO_LONG_MAP[@]}"; do
-        # strip trailing colon(s)
-        __PO__opt="${__PO__key%%:*}"
-        __PO__variable="${PO_LONG_MAP[$__PO__key]}"
-        if [[ "$1" == "--$__PO__opt" ]]; then
-            if [[ "$__PO__opt" == "$__PO__key" ]]; then
-                eval ${__PO__variable}="true"
-                shift
-                continue 2
-            else
-                __PO__set_var_with_default "${__PO__variable}" "$2"
-                shift 2
-                continue 2
-            fi
-        elif [[ "$__PO__opt" == "$__PO__key" && "$1" == "--no-$__PO__opt" ]]; then
-            eval ${__PO__variable}="false"
+
+    declare -A inverses
+    for key in "${!PO_LONG_MAP[@]}"; do
+        if [[ "${PO_LONG_MAP[$key]%=*}" != "${PO_LONG_MAP[$key]}" && \
+            "${key%::}" == "${key}" ]]; then
+            # sanity failure!
+            echo "PANIC: non-optional key '$key' must not have a default value" >&2
+            exit 4
+        fi
+        if [[ "$key" == "${key%:}" ]]; then
+            # key takes no value, therefore we can support no-<key>
+            inverses["no-$key"]="${PO_LONG_MAP[$key]}"
+        fi
+    done
+
+    # concatenate option hash keys and invoke enhanced getopt on ARGV
+    local canonical_args
+    ! canonical_args=$(getopt \
+        -o $(IFS="";echo "${!PO_SHORT_MAP[*]}") \
+        -l $(IFS=,;echo "${!PO_LONG_MAP[*]}","${!inverses[*]}") \
+        --name "$0" -- "$@")
+    if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+        exit 2
+    fi
+
+    # `set` reloads ARGV; `--` forbids set from consuming options
+    echo set -- "$canonical_args"
+}
+
+__PO__parse_argv() {
+    local key
+    local opt
+    local variable
+    while true; do
+        if [[ "$1" == "--" ]]; then
+            # stop processing options
             shift
-            continue 2
+            break
         fi
+        for key in "${!PO_SHORT_MAP[@]}"; do
+            # strip trailing colon(s) if possible
+            # we can test opt==key below to see if the option expects a value
+            opt="${key%%:*}"
+            variable="${PO_SHORT_MAP[$key]}"
+            if [[ "$1" == "-$opt" ]]; then
+                if [[ "$opt" == "$key" ]]; then
+                    __PO__set_var "${variable}" "true"
+                    shift
+                    continue 2
+                else
+                    __PO__set_var "${variable}" "$2"
+                    shift 2
+                    continue 2
+                fi
+            fi
+        done
+        for key in "${!PO_LONG_MAP[@]}"; do
+            # strip trailing colon(s) if possible
+            # we can test opt==key below to see if the option expects a value
+            opt="${key%%:*}"
+            variable="${PO_LONG_MAP[$key]}"
+            if [[ "$1" == "--$opt" ]]; then
+                if [[ "$opt" == "$key" ]]; then
+                    __PO__set_var "${variable}" "true"
+                    shift
+                    continue 2
+                else
+                    __PO__set_var "${variable}" "$2"
+                    shift 2
+                    continue 2
+                fi
+            elif [[ "$1" == "--no-$opt" ]]; then
+                if [[ "$opt" == "$key" ]]; then
+                    __PO__set_var "${variable}" "false"
+                    shift
+                    continue 2
+                else
+                    echo "PANIC when parsing options, aborting" >&2
+                    exit 3
+                fi
+            fi
+        done
+        echo "PANIC when parsing options, aborting" >&2
+        exit 3
     done
-    echo "PANIC when parsing options, aborting"
-    exit 3
-done
+
+    # return the remaining arguments
+    # `set` reloads ARGV; `--` forbids set from consuming options
+    echo set -- "$@"
+}
+
+eval $(__PO__canonicalize_argv "$@")
+eval $(__PO__parse_argv "$@")
