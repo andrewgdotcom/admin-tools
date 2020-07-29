070cd44767cabb9888300f4e0f05da5139ba502e
Andrew Gallagher <andrew.gallagher@siren.io>
2020-07-29 14:56:17 +0100

    Migrate allow-me to poshlib, tidy up vercomp

---
+++ allow-me.sh
@@ -0,0 +1,26 @@
+# A quick script to explicitly allow ssh connections in from the calling IP
+    
+allow-me() { (
+    SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+    . ${SCRIPT_DIR}/poshlib/poshlib.sh
+    use swine
+    use parse-opt
+    
+    PO_SIMPLE_PARAMS="COMMENT"
+    eval $(parse-opt-simple)
+    
+    if [[ ! $COMMENT ]]; then
+        COMMENT="added by $0"
+    fi
+    
+    if [[ $@ ]]; then
+        for source in $@; do
+            ufw allow to any app OpenSSH from "${source}" comment "${COMMENT}"
+        done
+    elif [[ ${SSH_CONNECTION:-} ]]; then
+        ssh_connection_array=($SSH_CONNECTION)
+        ufw allow to any app OpenSSH from "${ssh_connection_array[0]}" comment "${COMMENT}"
+    else
+        die 1 "No IPs supplied and could not guess connection details"
+    fi
+) }
