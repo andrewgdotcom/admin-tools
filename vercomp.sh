89f181adca00048b39458d3b63ae0180adcf42ec
Andrew Gallagher <andrew.gallagher@siren.io>
2020-10-07 15:55:51 +0100

    use subshell; handle missing key/value

--- vercomp.sh
+++ vercomp.sh
@@ -3,14 +3,16 @@
 
 # Exits with code 0 for equality, 1 when $1 > $2 and 2 when $1 < $2
 
-vercomp() {
+vercomp() { (
+    use swine
+
     if [[ $1 == $2 ]]
     then
         return 0
     fi
-    IFS=.
-    ver1=($1)
-    ver2=($2)
+    local IFS=.
+    local ver1=($1)
+    local ver2=($2)
     # fill empty fields in ver1 with zeros
     for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
     do
@@ -18,10 +20,10 @@ vercomp() {
     done
     for ((i=0; i<${#ver1[@]}; i++))
     do
-        if [[ -z ${ver2[i]} ]]
+        if [[ -z ${ver2[$i]:-} ]]
         then
             # fill empty fields in ver2 with zeros
-            ver2[i]=0
+            ver2[$i]=0
         fi
         if ((10#${ver1[i]} > 10#${ver2[i]}))
         then
@@ -32,5 +34,5 @@ vercomp() {
             return 2
         fi
     done
-    return 0
-}
+    exit 0
+) }
